42d68c69a510a56a2eb369c54d5056f5bd9e88d7
Fabian Tauriello <fabian@Fabians-MacBook-Pro.local>
2019-11-15 19:41:05 +0000

    Update portfolio

--- js/main.js
+++ js/main.js
@@ -1,25 +1,39 @@
-//-DOM ELEMENT CAPTURE------------------------------------------------------------------------------------------
+// navbar height
+const navHeight = '10vh';
 
 // get nav bar
-const navbar = document.querySelector('#navbar')
+const navbar = document.querySelector('#navbar');
 
+// get offsettop of navbard
 const sticky = navbar.offsetTop;
 
-//-EVENTS--------------------------------------------------------------------------------------------------------
+// get sections
+const aboutSection = document.getElementById('about');
+
+// get navbar links
+const aboutLink = document.getElementById('aboutLink');
+
+//-EVENTS AND FUNCTIONS--------------------------------------------------------------------------------------------------------
 
 
-// when the user scrolls the page, execute fixNavBar function
-window.onscroll = function() {fixNavBar()};
+aboutLink.onclick = function() {
+    
+};
+window.onscroll = function() {
+    fixNavBar()
+};
+window.onload = function() {
+    
+};
 
-//-FUNCTIONS-----------------------------------------------------------------------------------------------------
 
 // function - add the sticky class to the navbar when its scroll position is reached. Remove "sticky" when the scroll position is left.
 function fixNavBar() {
     if(window.pageYOffset > sticky) {
-        navbar.classList.add('sticky');
+        navbar.classList.add('fixed-top');
+        document.getElementById('main').style.paddingTop = '10vh';
     } else {
-        navbar.classList.remove('sticky');
+        navbar.classList.remove('fixed-top');
+        document.getElementById('main').style.paddingTop = '0vh';
     }
-}
-
-//-------------------------------------------------------------------------------------------
+};
