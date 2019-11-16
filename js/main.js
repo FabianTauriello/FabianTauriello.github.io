a3d92ee663c6a6d594f666966d7884c806652851
Fabian Tauriello <fabian@Fabians-MacBook-Pro.local>
2019-11-16 01:59:11 +0000

    Update portfolio

--- js/main.js
+++ js/main.js
@@ -2,36 +2,30 @@
 const navHeight = '10vh';
 
 // get nav bar
-const navbar = document.querySelector('#navbar');
+let navbar = document.getElementById('navbar');
 
-// get offsettop of navbard
-const sticky = navbar.offsetTop;
-
-// get sections
-const aboutSection = document.getElementById('about');
-
-// get navbar links
-const aboutLink = document.getElementById('aboutLink');
+// get offsettop of navbar
+let sticky = navbar.offsetTop;
 
 //-EVENTS AND FUNCTIONS--------------------------------------------------------------------------------------------------------
 
 
-aboutLink.onclick = function() {
-    
+aboutLink.onclick = function () {
+
 };
-window.onscroll = function() {
+window.onscroll = function () {
     fixNavBar()
 };
-window.onload = function() {
-    
+window.onload = function () {
+    console.log(navbar)
 };
 
 
 // function - add the sticky class to the navbar when its scroll position is reached. Remove "sticky" when the scroll position is left.
 function fixNavBar() {
-    if(window.pageYOffset > sticky) {
+    if (window.pageYOffset > sticky) {
         navbar.classList.add('fixed-top');
-        document.getElementById('main').style.paddingTop = '10vh';
+        document.getElementById('main').style.paddingTop = '8vh';
     } else {
         navbar.classList.remove('fixed-top');
         document.getElementById('main').style.paddingTop = '0vh';
