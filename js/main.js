36146353b8d2dbb14b3933eb0b930994375d7840
Fabian Tauriello <fabian@Fabians-MacBook-Pro.local>
2019-11-18 19:01:43 +0000

    First update without bootstrap

--- js/main.js
+++ js/main.js
@@ -1,33 +1,41 @@
-// navbar height
-const navHeight = '10vh';
+// get navbar links and assign it to array
+const navbarlinks = document.getElementById('navbar').querySelectorAll('a');
 
-// get nav bar
-let navbar = document.getElementById('navbar');
+// add event same event listener to all navbar links 
+for (let i = 0; i < navbarlinks.length; i++) {
+    navbarlinks[i].addEventListener('click', changeActiveNavbarLink);
+}
 
 // get offsettop of navbar
-let sticky = navbar.offsetTop;
+// let sticky = navbar.offsetTop;
 
 //-EVENTS AND FUNCTIONS--------------------------------------------------------------------------------------------------------
 
 
-aboutLink.onclick = function () {
-
-};
 window.onscroll = function () {
-    fixNavBar()
+    // fixNavBar()
 };
 window.onload = function () {
-    console.log(navbar)
+
 };
 
+// Change the current active navbar link
+function changeActiveNavbarLink() {
+    // deactivate current navbar link
+    for (let i = 0; i < navbarlinks.length; i++) {
+        navbarlinks[i].classList.remove('active-nav-link');
+    }
+    // activate new navbar link
+    this.classList.add('active-nav-link');
+}
 
 // function - add the sticky class to the navbar when its scroll position is reached. Remove "sticky" when the scroll position is left.
-function fixNavBar() {
-    if (window.pageYOffset > sticky) {
-        navbar.classList.add('fixed-top');
-        document.getElementById('main').style.paddingTop = '8vh';
-    } else {
-        navbar.classList.remove('fixed-top');
-        document.getElementById('main').style.paddingTop = '0vh';
-    }
-};
+// function fixNavBar() {
+//     if (window.pageYOffset > sticky) {
+//         navbar.classList.add('fixed-top');
+//         document.getElementById('main').style.paddingTop = '8vh';
+//     } else {
+//         navbar.classList.remove('fixed-top');
+//         document.getElementById('main').style.paddingTop = '0vh';
+//     }
+// };
